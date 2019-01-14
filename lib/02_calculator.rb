b7444e2ff3179d82f89bdbe122a89207c0bbc766
Salome Marzin <salome.mrzn@gmail.com>
2019-01-14 15:25:05 +0100

    problem pending 02_calculator

--- lib/02_calculator.rb
+++ lib/02_calculator.rb
@@ -1,22 +1,32 @@
-def add (a,b)
+def add(a, b)
 	add_value = a + b
 	return add_value
 end
 
-def subtract (a,b)
+def subtract(a, b)
 	subtract_value = a - b
 	return subtract_value
 end
 
-def sum (values)
+def sum(values)
 	sum_value = 0
 	values.each do |value|
-		 sum_value += value
+		sum_value += value
 	end
 	return sum_value
 end
 
-def multiply (a,b)
+def multiply(a, b)
 	multiply_value = a * b
 	return multiply_value
+end
+
+def power(a, b)
+	power_value = a ** b
+	return power_value
+end
+
+def factorial (n)
+	factorial_value = (1..n).inject(:*)
+	return factorial_value
 end
